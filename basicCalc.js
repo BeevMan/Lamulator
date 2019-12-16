831afa9a4ac9d40ddba7e35ef4336eaa98e5b78c
BeevMan <52551233+BeevMan@users.noreply.github.com>
2019-12-16 01:57:43 -0600

    Add files via upload

--- basicCalc.js
+++ basicCalc.js
@@ -3,8 +3,7 @@
 let arrCurEqua = [];
 let arrHistory = [];
 let strCurNum = "0"; // switches to number after it returns an answer
-let intCount = 0; // goes up when math symbols are input
-let objMath = { 
+const objMath = { 
 	    "÷": function (a,b) {return a / b},
 		"X": function (a,b) {return a * b},
 		"-": function (a,b) {return a - b},
@@ -16,22 +15,17 @@ let objMath = {
 			let intTempb = Number(floatB) * decs;
 			console.log(intTemp,arrCurEqua[intSymbol],intTempb);
 			let temp = objMath[arrCurEqua[intSymbol]](intTemp,intTempb);
-			console.log(temp);
 			if (arrCurEqua[intSymbol] === "÷"){
-				console.log("floating point with division!!!");
 			    return temp;
 			} else if (arrCurEqua[intSymbol] === "X"){
-				console.log("floating point with multiplication!!");
 				temp = objMath["÷"](temp,decs);
 			}
-			
 			return temp = objMath["÷"](temp,decs);
 		}
 	};
 
 function calculate(input){
 	console.log("-----input pressed-------");
-	
 	
 	if (typeof input === "number" || input === "." && strCurNum.includes(".") === false){
 		if (strCurNum === "0" && typeof input === "number" || typeof strCurNum === "number"){ // leave the zero only when a decimal is input after 0
@@ -41,43 +35,35 @@ function calculate(input){
 		console.log(strCurNum);
 		return document.getElementById("equation").value = strCurNum;
 	} else if (objMath.hasOwnProperty(input)) { // if it's a math symbol
-	    if (objMath.hasOwnProperty(arrCurEqua[intCount]) && strCurNum === "0"){
+	    if (objMath.hasOwnProperty(arrCurEqua[arrCurEqua.length-1]) && strCurNum === "0"){ 
 			console.log("replaced symbol!");
-			return arrCurEqua[intCount] = input;
+			return arrCurEqua[arrCurEqua.length-1] = input;
 		}
 		if (typeof strCurNum === "number"){
 			strCurNum = strCurNum.toString();
 			console.log("converted an answer to string!");
 		}
-		if (intCount > 0){
-		    intCount++;
-	    }
-		
-		console.log(intCount);
-	    arrCurEqua[intCount] = strCurNum;
+	    arrCurEqua.push(strCurNum);
 		strCurNum = "0";
-		arrCurEqua[++intCount] = input;
-		console.log(intCount);
+		arrCurEqua.push(input);
 		
-	} else if (input === "=" && intCount >= 1 && strCurNum !== "0"){ 
-	    console.log("equals!");
-		arrCurEqua[++intCount] = strCurNum;
+	} else if (input === "=" && arrCurEqua.length >= 2 && strCurNum !== "0"){ 
+		arrCurEqua.push(strCurNum);
 		strCurNum = "0";
-		arrCurEqua[++intCount] = input;
+		arrCurEqua.push(input);
 		
 		let num1 = "";
 		let intb = "";
 		let intSymbol = 1;
 		let num2 = 2;
-		for (var i = 2; i <= intCount; i++){
-			console.log("LOOPY!",i,intCount);
+		for (var i = 2; i <= arrCurEqua.length; i++){
+			console.log("Doing Equation!");
 			if (arrCurEqua[intSymbol] === "=") {
-				console.log("break!", i,intCount);
-				arrCurEqua[++intCount] = num1.toString();
+				console.log("end of equation");
+				arrCurEqua.push(num1.toString());
 				arrHistory.push(arrCurEqua);
 				arrCurEqua = [];
 				strCurNum = num1;
-				intCount = 0;
 				return document.getElementById("equation").value = strCurNum;
 			};
 			if (typeof num1 === "string"){
@@ -95,8 +81,7 @@ function calculate(input){
 				if (arrCurEqua[num2].indexOf(".") !== -1){
 					len2 = arrCurEqua[num2].length-1 - arrCurEqua[num2].indexOf(".");
 				}
-                let decCount = 0;
-                console.log("len1",len1,"len2",len2);				
+                let decCount = 0;			
 				if (len1 >= len2){ 
 					decCount = len1;
 				} else {
@@ -112,21 +97,17 @@ function calculate(input){
 			}
 			intSymbol += 2;
 			num2 += 2;
-			console.log(num1);
-		}
-		
+			console.log("=",num1);
+		}	
 	}
-
 }
 
 function clearEntry (){
-	console.log("display and strCurNum cleared")
 	strCurNum = "0";
 	return document.getElementById("equation").value = strCurNum;
 }
 function clearIt (){
 	arrCurEqua = [];
-	intCount = 0;
     clearEntry();
 }
 function backspace(){ 
@@ -137,6 +118,12 @@ function backspace(){
 	return document.getElementById("equation").value = strCurNum;
 }
 
-
-
-
+/* --IF DIVIDING WHOLE NUMBERS RETURNS AN ANSWER WITH DECIMALS THEIR MAY BE PROBLEMS--
+50/3 currently returns 16.666666666666668 IT should return 16.666666666666667 but when multiplying my anser from 50/3 by 3 it returns 50
+1/6 currently wrong, returns 0.16666666666666666 instead of 0.1666666666666667? when multiplying the answer by 6 it returns back to 1
+1/7 currently returns 0.14285714285714285 instead of 0.1428571428571429?
+1/9 currently returns 0.1111111111111111 (times 9 afterwards does not return 1)instead of 0.1111111111111111
+10/12 is wrong and doesn't return to 10 when asnwer is multiplied by 12
+10/15 is wrong but returns 10 when answer is multiplied by 15
+10/24 returns the correct answer but when multiplying the answer by 24 it does not return 10
+*/
